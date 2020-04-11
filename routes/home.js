508fbaaa8365cfaa04077b7485673f50e4d54baf
Sebastian Perez <sebastian.perez@23people.io>
2020-04-11 02:37:37 -0400

    Lios resulto con las api

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,26 @@
 var express = require('express');
-var request = require('request');
 var router = express.Router();
+const request = require('request');
 
+var api_url = 'https://api.covid19api.com/countries';
 
+router.get('/', function (req, res, next) {
+    request(api_url, { json: true }, (err, res, body) => {
+        if (err) {
+            console.log(err);
+        }
+        var countries = body;
+        console.log(countries);
+        countries.forEach(element => {
+            console.log(element["Country"]);
+        });
+        
+        
+        
+    });
 
-router.get('/', function(req, res, next) {
+    res.render('home/welcome', { sitename: 'CovidApp' });
 
-    res.render('home/welcome', {sitename: 'CovidApp'});
 });
 
 module.exports = router;
-
-
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const request = require('request');
+var request = require('request');
 
 
-_EXTERNAL_URL = 'https://api.covid19api.com/countries';
+api_url = 'https://api.covid19api.com/dayone/country/chile/status/confirmed';
 
 router.get('/', function (req, res, next) {
-    request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
+    request(api_url, { json: true }, (err, res, body) => {
         if (err) {
             console.log(err);
         }
-        var countries = body;
-        console.log(body);
+        var summary = body;        
         
     });
 
 
     res.render('home/users', {
-        users: 'PAISES'
+        users: 'Country Summary'
     });
 
 });
