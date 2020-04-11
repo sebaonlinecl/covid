43dce4483aa61d5ce49a69f39aacb110a66142d9
Sebastian Perez <sebastian.perez@23people.io>
2020-04-11 00:11:00 -0400

    Paises

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
-router.get('/', function(req, res, next) {
-    res.render('home/users', {users: 'Usuarios'});
+
+_EXTERNAL_URL = 'https://api.covid19api.com/countries';
+
+router.get('/', function (req, res, next) {
+    request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
+        if (err) {
+            console.log(err);
+        }
+        var countries = body;
+        console.log(body);
+        
+    });
+
+
+    res.render('home/users', {
+        users: 'PAISES'
+    });
 
 });
 
